2e0dfa14b61aa06327381a5191b8e2007d98d8d2
danielpowell4 <danielpowell12@msn.com>
2016-03-11 14:40:18 -0700

    basic exercise list

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+var exerciseList = [
+  {category: 'warm_up', exercise: 'Warm Up 1', intensity: 5}
+  {category: 'plyo', exercise: 'Plyo 1', intensity: 5},
+  {category: 'push', exercise: 'Push 1', intensity: 5},
+  {category: 'pull', exercise: 'Pull 1', intensity: 5},
+  {category: 'core', exercise: 'Core 1', intensity: 5},
+  {category: 'legs', exercise: 'Legs 1', intensity: 5},
+  {category: 'cool_down', exercise: 'Cool Down 1', intensity: 5}
+];
